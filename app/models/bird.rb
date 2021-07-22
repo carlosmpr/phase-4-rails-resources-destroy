488be6a4c2c76d5ada2579469b6db41f01c730ef
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-07-22 09:56:07 -0400

    Completed

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,11 @@
 class Bird < ApplicationRecord
+    def destroy
+        bird = Bird.find_by(id: params[:id])
+        if bird
+          bird.destroy
+          head :no_content
+        else
+          render json: { error: "Bird not found" }, status: :not_found
+        end
+      end 
 end
